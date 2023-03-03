3baa1e9767c43cbdebec51ba7444f3276d33abc0
kethllynanjos <kekeh.gabrielle10@gmail.com>
2023-03-02 22:29:39 -0300

    aula4

---
+++ controlelab/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/principal', function () {
+    return view('principal');
+});
+
+Route::get('/laboratorio', function () {
+    return view('laboratorio');
+});
+
+Route::get('/contato', function () {
+    return view('contato');
+});
+
+Route::get('/reclamacao', function () {
+    return view('reclamacao');
+});
+
+Route::get('/quemsomos', function () {
+    return view('quemsomos');
+});
